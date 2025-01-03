97ce949dceb0ac70af0f6e777a8936a4d4bf4d80
Koga_hikaru <90139340+hikaru0331@users.noreply.github.com>
2025-01-04 00:47:33 +0900

    add :sparkles: Modelの作成

--- Assets/Scripts/TimerModel.cs
+++ Assets/Scripts/TimerModel.cs
@@ -1,9 +1,28 @@
 using System;
+using UniRx;
 
 public class TimerModel
 {
-    private float _timer;
-    public float Timer => _timer;
+    private readonly ReactiveProperty<float> _timer;
+    public IReadOnlyReactiveProperty<float> Timer => _timer;
     
+    public TimerModel()
+    {
+        _timer = new ReactiveProperty<float>(0);
+    }
     
+    public void ResetTimer()
+    {
+        _timer.Value = 0;
+    }
+    
+    public void UpdateTimer(float deltaTime)
+    {
+        _timer.Value += deltaTime;
+        
+        if (_timer.Value >= 10)
+        {
+            _timer.Value = 0;
+        }
+    }
 }
